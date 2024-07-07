967e7e5c21bb6c562dd9fd12ff9b159a12e369ed
Puru Jindal <105160314+Puru-6212@users.noreply.github.com>
2024-07-07 21:03:38 +0530

    Update uart_sender.c
    Issues regarding led blinking to stop, when signal is removed is resolved.

--- Raspberry_Pi_Pico/uart_sender.c
+++ Raspberry_Pi_Pico/uart_sender.c
@@ -10,12 +10,21 @@
 #define SIGNAL_PIN 15   
 
 volatile int blink_interval_index = 0;
-const char* blink_intervals[] = {"1", "2", "3", "4"}; 
+const char* blink_intervals[] = {"1", "2", "3", "4"};  
+bool signal_active = false;
 
 void gpio_callback(uint gpio, uint32_t events) {
     if (gpio == SIGNAL_PIN) {
-        uart_puts(UART_ID, blink_intervals[blink_interval_index]);
-        blink_interval_index = (blink_interval_index + 1) % 4;  
+        if (gpio_get(SIGNAL_PIN)) {
+            // Pin is high, send blink interval
+            uart_puts(UART_ID, blink_intervals[blink_interval_index]);
+            blink_interval_index = (blink_interval_index + 1) % 4;  // Cycle through intervals
+            signal_active = true;
+        } else if (signal_active) {
+            // Pin is low, send stop signal
+            uart_puts(UART_ID, "S");
+            signal_active = false;
+        }
     }
 }
 
@@ -30,8 +39,7 @@ int main() {
     gpio_set_dir(SIGNAL_PIN, GPIO_IN);
     gpio_pull_down(SIGNAL_PIN);  
 
-    // Set up GPIO interrupt
-    gpio_set_irq_enabled_with_callback(SIGNAL_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(SIGNAL_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     while (true) {
         tight_loop_contents(); 
